ac4c734c17f7e855b90ea557c43272eec8cf36dd
Robeen Good <beezoomx@gmail.com>
2023-12-05 15:10:18 +0700

    add new solution

--- randomes/calculations/greatest_product.py
+++ randomes/calculations/greatest_product.py
@@ -16,11 +16,22 @@ from operator import mul
 from functools import reduce
 
 
-def greatest_product(st: str) -> int:
+def greatest_product1(st: str) -> int:
     """
     Максимальное значение произведений цифр подстроки.
     """
     return max(reduce(mul, map(int, st[x:x + 5])) for x in range(len(st) - 4))
+
+
+def greatest_product2(st: str) -> int:
+    """
+    Максимальное значение произведений цифр подстроки.
+    """
+    r, w, z = 0, 0, 1
+    for i, x in enumerate(st):
+        w, z = [w, i + 5][x == '0'], [z * int(x), 1][x == '0']
+        r = [r, max(r, z := z // [1, int(st[i - 5])][4 < i and w < i])][3 < i and w <= i]
+    return r
 
 
 def test() -> None:
@@ -35,7 +46,8 @@ def test() -> None:
         ("02494037820244202221011110532909999", 0),
     )
     for key, val in data:
-        assert greatest_product(key) == val
+        assert greatest_product1(key) == val
+        assert greatest_product2(key) == val
 
 
 if __name__ == '__main__':
