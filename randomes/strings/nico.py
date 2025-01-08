e0abcfe5eb56039bcb696f79e8f66330dad6c56d
Robeen Good <beezoomx@gmail.com>
2025-01-09 00:43:51 +0700

    add new solution

--- randomes/strings/nico.py
+++ randomes/strings/nico.py
@@ -28,21 +28,22 @@ o n
 c s e e r
 n t i o f
 a r m i t
-  o n   
+  o n
 
 Примеры
 
 nico("crazy", "secretinformation") => "cseerntiofarmit on  "
 nico("abc", "abcd") => "abcd  "
-nico("ba", "1234567890") => "2143658709" 
-nico("key", "key") => "eky" 
+nico("ba", "1234567890") => "2143658709"
+nico("key", "key") => "eky"
 """
 import typing
+import unittest
 from math import ceil
 from operator import truediv
 
 
-def nico(key: str, message: str) -> str:
+def nico1(key: str, message: str) -> str:
     """
     Шифрует строку по заданному ключу.
     """
@@ -54,22 +55,31 @@ def nico(key: str, message: str) -> str:
     return ''.join(res)
 
 
-def test(func: typing.Callable, data: tuple[tuple[typing.Any, typing.Any]]) -> None:
+def nico2(key: str, msg: str) -> str:
+    """
+    Шифрует строку по заданному ключу.
+    """
+    x = (m := len(msg)) // (k := len(key)) + bool(m % k)
+    return ''.join([''.join([x for _, x in sorted(zip(key, f'{msg[n * k:n * k + k]:<{k}}'))]) for n in range(x)])
+
+
+def test(funcs: tuple[typing.Callable], data: tuple[tuple[typing.Any, typing.Any]]) -> None:
     """Тестирование работы алгоритмов с помощью unittest."""
-    import unittest
 
     def test_func(func: typing.Callable, key: typing.Any, val: typing.Any) -> typing.Callable:
         """Создает кейсы для тестирования."""
         return lambda self: self.assertEqual(func(*key), val)
 
-    funcs = {f'test_{i}': test_func(func, key, val) for i, (key, val) in enumerate(data, 1)}
-    suite = unittest.TestLoader().loadTestsFromTestCase(type('Tests', (unittest.TestCase,), funcs))
+    for n, func in enumerate(funcs, 1):
+        funcs = {f'test_{i}': test_func(func, key, val) for i, (key, val) in enumerate(data, 1)}
+        suite = unittest.TestLoader().loadTestsFromTestCase(
+            type(f'Tests_{n}', (unittest.TestCase,), funcs))
 
-    unittest.TextTestRunner().run(suite)
+        unittest.TextTestRunner().run(suite)
 
 
 if __name__ == '__main__':
-    test(nico, (
+    test((nico1, nico2), (
         (("crazy", "secretinformation"), "cseerntiofarmit on  "),
         (("abc", "abcd"), "abcd  "),
         (("ba", "1234567890"), "2143658709"),
