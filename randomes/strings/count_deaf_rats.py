ffae8adc2b8dfdda9d9e2a6dba9ad47213d2e91e
Robeen Good <beezoomx@gmail.com>
2025-01-18 23:02:10 +0700

    add unittest

--- randomes/strings/count_deaf_rats.py
+++ randomes/strings/count_deaf_rats.py
@@ -23,6 +23,8 @@
     ex3 ~O~O~O~OP~O~OO~ есть 2 глухие крысы
 
 """
+import typing
+import unittest
 
 
 def count_deaf_rats(s: str):
@@ -32,18 +34,22 @@ def count_deaf_rats(s: str):
     return sum([x[i*2:i*2+2] for i in range(len(x)//2)].count('~O'[::[-1, 1][i]]) for i, x in enumerate(s.replace(' ', '').split('P')))
 
 
-def test() -> None:
-    """
-    Тестирование работы алгоритмов.
-    """
-    data = (
-        ("~O~O~O~O P", 0),
-        ("P O~ O~ ~O O~", 1),
-        ("~O~O~O~OP~O~OO~", 2),
-    )
-    for key, val in data:
-        assert count_deaf_rats(key) == val
+def test(func: typing.Callable, data: tuple[tuple[typing.Any, typing.Any]]) -> None:
+    """Тестирование работы алгоритмов с помощью unittest."""
+
+    def test_func(func: typing.Callable, key: typing.Any, val: typing.Any) -> typing.Callable:
+        """Создает кейсы для тестирования."""
+        return lambda self: self.assertEqual(func(key), val)
+
+    funcs = {f'test_{i}': test_func(func, key, val) for i, (key, val) in enumerate(data, 1)}
+    suite = unittest.TestLoader().loadTestsFromTestCase(type('Tests', (unittest.TestCase,), funcs))
+
+    unittest.TextTestRunner().run(suite)
 
 
 if __name__ == '__main__':
-    test()
+    test(count_deaf_rats, (
+        ("~O~O~O~O P", 0),
+        ("P O~ O~ ~O O~", 1),
+        ("~O~O~O~OP~O~OO~", 2),
+    ))
