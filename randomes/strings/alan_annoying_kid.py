0ffa0159574afc333f1ac2146b66ebb6759f8105
rgeex <beezoomx@gmail.com>
2025-10-27 14:35:33 +0700

    change test

--- randomes/strings/alan_annoying_kid.py
+++ randomes/strings/alan_annoying_kid.py
@@ -43,6 +43,8 @@ output = "I don't think you didn't play football today, I think you did play it!
 input  = "Today I cleaned the kitchen."
 output = "I don't think you cleaned the kitchen today, I think you didn't clean at all!"
 """
+import typing
+import unittest
 
 
 def alan_annoying_kid(s: str) -> str:
@@ -55,20 +57,24 @@ def alan_annoying_kid(s: str) -> str:
     return f"I don't think you {a} today, I think you did{b}!"
 
 
-def test() -> None:
-    """
-    Тестирование работы алгоритмов.
-    """
-    data = (
+def test(func: typing.Callable, data: tuple[tuple[typing.Any, typing.Any]]) -> None:
+    """Тестирование работы алгоритмов с помощью unittest."""
+
+    def test_func(func: typing.Callable, key: typing.Any, val: typing.Any) -> typing.Callable:
+        """Создает кейсы для тестирования."""
+        return lambda self: self.assertEqual(func(key), val)
+
+    funcs = {f'test_{i}': test_func(func, key, val) for i, (key, val) in enumerate(data, 1)}
+    suite = unittest.TestLoader().loadTestsFromTestCase(type('Tests', (unittest.TestCase,), funcs))
+
+    unittest.TextTestRunner().run(suite)
+
+
+if __name__ == '__main__':
+    test(alan_annoying_kid, (
         ("Today I played football.","I don't think you played football today, I think you didn't play at all!"),
         ("Today I didn't play football.","I don't think you didn't play football today, I think you did play it!"),
         ("Today I didn't attempt to hardcode this Kata.","I don't think you didn't attempt to hardcode this Kata today, I think you did attempt it!"),
         ("Today I cleaned the kitchen.","I don't think you cleaned the kitchen today, I think you didn't clean at all!"),
         ("Today I learned to code like a pro.","I don't think you learned to code like a pro today, I think you didn't learn at all!"),
-    )
-    for key, val in data:
-        assert alan_annoying_kid(key) == val
-
-
-if __name__ == '__main__':
-    test()
+    ))
