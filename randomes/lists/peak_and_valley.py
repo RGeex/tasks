d9ba1a6b2f7af84ea37b636ee495edd0f29927da
rgeex <beezoomx@gmail.com>
2026-03-18 15:50:30 +0700

    add new solution

--- randomes/lists/peak_and_valley.py
+++ randomes/lists/peak_and_valley.py
@@ -40,7 +40,6 @@ import unittest
 from typing import Any, Callable, List, Tuple
 
 
-
 def peak_and_valley(arr: List[int], x: int = 3) -> List[int]:
     """
     Определяет пиковые значения.
@@ -59,6 +58,13 @@ def peak_and_valley(arr: List[int], x: int = 3) -> List[int]:
     return res
 
 
+def peak_and_valley_2(arr: List[int]) -> List[int]:
+    """
+    Определяет пиковые значения.
+    """
+    return [x[3] for x in zip(*[arr[i:] for i in range(7)]) if x[3] < min(x[:3] + x[-3:]) or x[3] > max(x[:3] + x[-3:])]
+
+
 def test(func: Callable[[Any], Any], data: Tuple[Tuple[Any, Any], ...]) -> None:
     """Тестирование работы алгоритмов с помощью unittest."""
 
@@ -74,6 +80,15 @@ def test(func: Callable[[Any], Any], data: Tuple[Tuple[Any, Any], ...]) -> None:
 
 if __name__ == '__main__':
     test(peak_and_valley, (
+        ([10, 20, 30, 40, 30, 20, 10, 11, 12, 13, 14, 15, 16, 15, 14, 13], [40, 10, 16]),
+        ([50, 84, 49, 47, 80, 87, 87, 53, 76, 30, 10], [47]),
+        ([45, 94, 41, 76, 29, 96, 28, 13, 84, 69, 25], [96, 13]),
+        ([1, 16, 63, 78, 53, 78, 42, 39, 46, 88, 49, 96, 58, 82], [39]),
+        ([45, 75, 47, 44, 93, 95, 31, 99, 49, 48, 76, 2, 92, 23, 26, 19, 60, 45, 51], [31, 99, 2, 92, 19]),
+        ([49, 97, 76, 56, 96, 88, 65, 20, 14, 93, 32], []),
+    ))
+
+    test(peak_and_valley_2, (
         ([10, 20, 30, 40, 30, 20, 10, 11, 12, 13, 14, 15, 16, 15, 14, 13], [40, 10, 16]),
         ([50, 84, 49, 47, 80, 87, 87, 53, 76, 30, 10], [47]),
         ([45, 94, 41, 76, 29, 96, 28, 13, 84, 69, 25], [96, 13]),
