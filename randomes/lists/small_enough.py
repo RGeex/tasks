a0e9958158a0c4a3517e3fbeaa99551ff55b0f56
rgeex <beezoomx@gmail.com>
2026-09-24 13:53:32 +0700

    add new solution

--- randomes/lists/small_enough.py
+++ randomes/lists/small_enough.py
@@ -16,6 +16,13 @@ def small_enough(array: List[int], limit: int) -> bool:
     return not next((1 for n in array if n > limit), 0)
 
 
+def small_enough_2(array: List[int], limit: int) -> bool:
+    """
+    Проверяет все ли числа из списка не превышают лимит.
+    """
+    return all(map(limit.__ge__, array))
+
+
 def test(func: Callable[[Any], Any], data: Tuple[Tuple[Any, Any], ...]) -> None:
     """Тестирование работы алгоритмов с помощью unittest."""
 
@@ -31,6 +38,16 @@ def test(func: Callable[[Any], Any], data: Tuple[Tuple[Any, Any], ...]) -> None:
 
 if __name__ == '__main__':
     test(small_enough, (
+        ([[66, 101], 200], True),
+        ([[78, 117, 110, 99, 104, 117, 107, 115], 100], False),
+        ([[101, 45, 75, 105, 99, 107], 107], True),
+        ([[80, 117, 115, 104, 45, 85, 112, 115], 120], True),
+        ([[1, 1, 1, 1, 1, 2], 1], False),
+        ([[78, 33, 22, 44, 88, 9, 6], 87], False),
+        ([[1, 2, 3, 4, 5, 6, 7, 8, 9], 10], True),
+        ([[12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12], 12], True),
+    ))
+    test(small_enough_2, (
         ([[66, 101], 200], True),
         ([[78, 117, 110, 99, 104, 117, 107, 115], 100], False),
         ([[101, 45, 75, 105, 99, 107], 107], True),
